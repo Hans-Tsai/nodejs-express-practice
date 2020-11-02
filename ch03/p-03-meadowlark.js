46189a13390a23cacdd4eb0c0ec7c904077deea9
Hans-Tsai <lgs840522@gmail.com>
2020-11-02 22:34:06 +0800

    完成ch03的所有練習

--- ch03/p-03-meadowlark.js
+++ ch03/p-03-meadowlark.js
@@ -6,17 +6,27 @@ const app = express();
 // 設定HandleBars view 引擎
 app.engine('handlebars', expressHandleBars({
   defaultLayout: 'main',
-}));
+}))
 app.set('view engine', 'handlebars');
 
-// express.static() 中介函式的效果相當於---為你想要傳遞的各個靜態檔案建立一個路由來算繪檔案,並將它回傳給用戶端
 app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.render('home'));
 
-app.get('/about', (req, res) => res.render('about'));
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+]
+
+app.get('/about', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+  res.render('about', { fortune: randomFortune });
+});
 
 // 自訂當回傳404錯誤時的頁面(這個作法省略res.type()了!)
 app.use((req, res) => {
@@ -26,11 +36,11 @@ app.use((req, res) => {
 
 // 自訂當回傳500錯誤時的頁面(這個作法省略res.type()了!)
 app.use((err, req, res, next) => {
-  console.log(err.message);
-  res.status(500);
-  res.render('500');
+    console.log(err.message);
+    res.status(500);
+    res.render('500');
 })
 
 app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`))
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`))
