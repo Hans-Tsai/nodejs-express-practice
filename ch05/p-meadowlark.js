0d87a670e48c010015b73a2e48f78930f7b9247f
Hans-Tsai <lgs840522@gmail.com>
2020-11-03 22:07:23 +0800

    調整about連結的功能成為可被require的模組 練習

--- ch05/p-meadowlark.js
+++ ch05/p-meadowlark.js
@@ -1,5 +1,5 @@
-const expressHandlebars = require('express-handlebars');
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 
 const handlers = require('./lib/handlers');
 
@@ -16,24 +16,26 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.home);
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => handlers.about);
+app.get('/about', handlers.about);
 
 // 自訂當回傳404錯誤時的頁面(這個作法省略res.type()了!)
 app.use(handlers.notFound);
 
 // 自訂當回傳500錯誤時的頁面(這個作法省略res.type()了!)
-app.use(handlers.serverError);
+app.use(handlers.serverError)
 
-if (require === module) {
-	app.listen(port, () => {
+// 將app修改成可以require的模組,因為原本app.listen()的設計只能直接執行
+if(require.main === module) {
+  app.listen(port, () => {
     console.log( `Express started on http://localhost:${port}` +
       '; press Ctrl-C to terminate.' )
   })
 } else {
-  module.exports = app;
+  module.exports = app
 }
+
 
 
 
