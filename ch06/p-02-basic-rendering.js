64115e3ed41578d30f3d2ca2b9bb6023c9fa4611
Hans-Tsai <lgs840522@gmail.com>
2020-11-07 11:44:05 +0800

    完成ch06 p-02練習

--- ch06/p-02-basic-rendering.js
+++ ch06/p-02-basic-rendering.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const app = express()
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
 
 // the following is needed to use views
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('handlebars', expressHandlebars({
+  defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
 
 // see the views/about.hbs file for the contents of this view
 app.get('/about', (req, res) => {
-  res.render('about')
-})
+  res.render('about');
+});
 
-app.get('*', (req, res) => res.send('Check out our "<a href="/about">About</a>" page!'))
+app.get('*', (req, res) => {
+  res.send('Check out our "<a href="/about">About</a>" page!')
+});
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/about\n`))
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/about\n`));
