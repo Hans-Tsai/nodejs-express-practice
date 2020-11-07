ac26841d5e52d4a660942b7280f9626fbe2bca58
Hans-Tsai <lgs840522@gmail.com>
2020-11-07 10:53:49 +0800

    完成ch06 p-00練習

--- ch06/p-00-echo-headers.js
+++ ch06/p-00-echo-headers.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/headers', (req, res) => {
+app.get('headers', (req, res) => {
   res.type('text/plain');
   const headers = Object.entries(req.headers)
     .map(([key, value]) => `${key}: ${value}`);
