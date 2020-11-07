e5d083913f32ce91865835bc9baa78a564b23711
Hans-Tsai <lgs840522@gmail.com>
2020-11-07 12:48:29 +0800

    更新ch06 p-06練習

--- ch06/p-06-custom-layout.js
+++ ch06/p-06-custom-layout.js
@@ -3,17 +3,15 @@ const expressHandlebars = require('express-handlebars');
 const app = express();
 
 // the following is needed to use views
-app.engine('handlerbars', expressHandlebars({
-  defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // 使用layout檔 views/custom.handlebars
 app.get('/custom-layout', (req, res) => {
   res.render('custom-layout', { layout: 'custom' })
 });
 
-app.get('*', (req, res) => res.send('Check out the "<a href="/no-layout">no layout</a>" page!'));
+app.get('*', (req, res) => res.send('Check out the "<a href="/custom-layout">custom layout</a>" page!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/no-layout\n`));
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/custom-layout\n`));
