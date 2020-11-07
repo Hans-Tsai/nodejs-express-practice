a484f7580043687f6e40bc433b5388e0b5194f65
Hans-Tsai <lgs840522@gmail.com>
2020-11-07 11:12:02 +0800

    完成ch06 p-01練習

--- ch06/p-01-disable-x-powered-by.js
+++ ch06/p-01-disable-x-powered-by.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.disable('x-powered-by')
+// 至少停用 X-Powered-By 標頭  => 可參考官方文件 <https://expressjs.com/zh-tw/advanced/best-practice-security.html>
+// 如果您不想使用 Helmet，最起碼請停用 X-Powered-By /標頭。攻擊者可能使用這個標頭（依預設，會啟用），來偵測執行 Express 的應用程式，然後啟動特定目標的攻擊。
+// 因此最佳作法是使用 app.disable() 方法來關閉標頭
+// 如果您使用 helmet.js，自會為您處理此事。   
+app.disable('x-powered-by');
 
 app.get('*', (req, res) => {
   res.send(`Open your dev tools and examine your headers; ` +
-    `you'll notice there is no x-powered-by header!`)
+  `you'll notice there is no x-powered-by header!`);
 })
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}\n`))
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}\n`));
