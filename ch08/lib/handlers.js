002918e3c2d8a9871275d98c0ac5b0e7ac3e7517
Hans-Tsai <lgs840522@gmail.com>
2020-11-10 22:04:26 +0800

    完成ch08的所有練習

--- ch08/lib/handlers.js
+++ ch08/lib/handlers.js
@@ -67,3 +67,4 @@ exports.notFound = (req, res) => res.render('404')
 /* eslint-disable no-unused-vars */
 exports.serverError = (err, req, res, next) => res.render('500')
 /* eslint-enable no-unused-vars */
+
