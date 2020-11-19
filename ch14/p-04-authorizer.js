d6f14a0be68e562fc6d8a3cad4579255ddf2a868
Hans-Tsai <lgs840522@gmail.com>
2020-11-19 22:06:43 +0800

    完成ch14 p-04 練習

--- ch14/p-04-authorizer.js
+++ ch14/p-04-authorizer.js
@@ -17,8 +17,44 @@ app.use(expressSession({
 }));
 
 // 以下的設定是為了可以使用views
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.engine('handlebars', expressHandlebars({ defaultLayout: '04-main' }));
 app.set('view engine', 'handlebars');
 
 // 為了圖片(images)和其他靜態檔案(static files)所做的設定
 app.use(express.static(__dirname + '/public'));
+
+// 這是假的登錄功能,並不會真的檢查使用者名稱和密碼
+app.post('/login', (req, res) => {
+  req.session.user = { email: req.body.email }
+  req.session.authorized = true
+  res.redirect('/secret')
+});
+
+// 假的登出功能
+app.get('/logout', (req, res) => {
+  delete req.session.user
+  delete req.session.authorized
+  res.redirect('/public')
+});
+
+// make the user object available to all views by putting it in the "locals" context
+app.use((req, res, next) => {
+  if(req.session) res.locals.user = req.session.user
+  next()
+});
+
+// 可以製作一個授權機制, 假設我們的用戶授權碼設定一個稱為req.session.authorized的session變數
+// 我們可以利用以下的函式來製作可重複使用的授權過濾器
+function authorize(req, res, next) {
+  if(req.session.authorized) return next()
+  res.render('not-authorized')
+};
+
+app.get('/public', (req, res) => res.render('public'));
+
+app.get('/secret', authorize, (req, res) => res.render('secret'));
+
+app.get('*', (req, res) => res.send('Check out the <a href="/public">public content</a>.'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/public`));
