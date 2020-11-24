70eb408730e916dcb90e7959b890053e0ff06a60
Hans Tsai <lgs840522@gmail.com>
2020-11-24 23:05:08 +0800

    更新ch16-practice的Vacation Component 利用按鈕綁定API並依據用戶的email是否已經註冊來切換web UI

--- ch16-practice/client/src/Vacation.js
+++ ch16-practice/client/src/Vacation.js
@@ -2,14 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function NotifyWhenInSeason({ sku }) {
-  return (
-    <>
-    <i>Notify me when this vacation is in season:</i>
-      <input type="email" placeholder="(your email)" />
-      <button>OK</button>
-    </>
+  const [registeredEmail, setRegisteredEmail] = useState(null)
+  const [email, setEmail] = useState('')
+  function onSubmit(event) {
+    fetch(`/api/vacation/${sku}/notify-when-in-season`, {
+        method: 'POST',
+        body: JSON.stringify({ email }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => {
+        if(res.status < 200 || res.status > 299)
+          return alert('We had a problem processing this...please try again.')
+        setRegisteredEmail(email)
+      })
+    event.preventDefault()
+  }
+  if(registeredEmail) return (
+    <i>You will be notified at {registeredEmail} when
+    this vacation is back in season!</i>
   )
-};
+  return (
+    <form onSubmit={onSubmit}>
+      <i>Notify me when this vacation is in season:</i>
+      <input
+        type="email"
+        placeholder="(your email)"
+        value={email}
+        onChange={({ target: { value } }) => setEmail(value)}
+        />
+      <button type="submit">OK</button>
+    </form>
+  )
+}
 
 function Vacation({ vacation }) {
   return (
