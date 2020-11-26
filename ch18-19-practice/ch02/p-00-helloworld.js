1eb9b5b45d3233718b17ad2e8363140d23b2e09f
Hans-Tsai <lgs840522@gmail.com>
2020-11-26 22:24:01 +0800

    新增ch18-19-practice/ 作為練習用資料夾

---
+++ ch18-19-practice/ch02/p-00-helloworld.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req,res) =>{
+  res.writeHead(200, {'Content-type': 'text/plain'});
+  res.end('Hello Hans');
+})
+
+server.listen(port, () => { console.log(`server started on port ${port}; ` + 
+'press Ctrl-C to terminate....';
+    )}
+)
